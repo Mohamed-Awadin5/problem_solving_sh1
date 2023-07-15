9d2de5318ac206da6bb8fd545176250905e4ff3f
Mohamed-Awadin5 <mohamed.awadin6@gmail.com>
2023-07-16 01:44:49 +0300

    first commit

---
+++ bin/mohamed.dart
@@ -0,0 +1,5 @@
+import 'package:mohamed/mohamed.dart' as mohamed;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mohamed.calculate()}!');
+}
---
+++ test/mohamed_test.dart
@@ -0,0 +1,8 @@
+import 'package:mohamed/mohamed.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
